63e84fac891fef598282a70d66c01ba0e2b34cce
bakinovsky-m <makbak@ya.ru>
2017-05-16 19:34:16 +0300

    init

---
+++ fill_tables.sql
@@ -0,0 +1,415 @@
+use mmorpg
+
+insert into races(name) values
+('human'),
+('elf'),
+('ork'),
+('undead'),
+('dwarf'),
+('halfling')
+go
+
+insert into fractions(name) values
+('aliance'),
+('horde'),
+('neutral'),
+('dominion'),
+('baknirs')
+go
+
+insert into talants(name, min_lvl) values
+('stunning', 1),
+('fatty', 2),
+('taunt', 0),
+('stealthy', 7),
+('speedy', 4)
+go
+
+insert into skills(name, min_lvl) values
+('firebolt', 1),
+('frost nova', 7),
+('mind control', 25),
+('stealth', 7),
+('arcane missiles', 3)
+go
+
+insert into inventories(size, fullness) values
+(17, 1),
+(25, 7),
+(48, 25),
+(9, 7),
+(14, 3)
+go
+
+insert into item_types(type_, feauture_name) values
+('bow', 'attack'),
+('sword', 'attack'),
+('shield', 'defense'),
+('helmet', 'defense'),
+('knife', 'attack'),
+('chainmail', 'defense')
+go
+
+insert into achievements(name) values
+('very rare achievement'),
+('easy-peasy'),
+('beat em up'),
+('100 rats are dead'),
+('dragonborn')
+go
+
+insert into quest_lines(name, description_) values
+('beginning', 'here you start your journey'),
+('i hate rats', 'oh dear, all my life is about fighting with rats'),
+('runner', 'would you like to work as courier for me?'),
+('party needs dd', 'lets go and train your skills in fighting'),
+('dragonslayer', 'now, ma man, you are trained enought to slay a dragon. the Dragon, to be clear')
+go
+
+insert into accounts(login_, password_, characters_count, max_characters, percentage_of_achievements) values
+('admin', 'admin', 0, 100, 0),
+('gamemaster', 'gamemaster', 0, 100, 0),
+('thechoosenone', 'alwaysontop', 10, 10, 100),
+('noob', 'noob', 1, 5, 0),
+('gaben', 'hl3confirmed', 2, 5, 24)
+go
+
+insert into monsters(name, lvl, health, attack, defense) values
+('rat', 1, 5, 1, 0),
+('gnoll', 3, 25, 3, 2),
+('angry fairy', 7, 35, 5, 5),
+('dragonfly', 15, 150, 20, 15),
+('Dragon', 50, 15000, 3000, 1000)
+go
+
+insert into items(name, type_, feature_value, min_lvl) values
+('wooden sword', 2, 1, 0),
+('golden sword', 2, 100, 50),
+('wooden shield', 3, 1, 0),
+('thieves knife', 5, 1, 0),
+('copper chainmail', 6, 1, 0),
+('bow of eternity', 1, 100, 50)
+go
+
+insert into quests(name, description_, reward_item, reward_money, lvl, quest_line, number_in_quest_line) values
+('dirty rat', 'kill one rat and i will reward you', NULL, 25, 1, 1, 1),
+('harvesting', 'harvest some wood', 1, 25, 1, 1, 2),
+('mining', 'mine 10 copper', 5, 25, 1, 1, 3),
+('Ogrimar is waiting for you', 'pls, deliver this package to the Mayor of the Ogrimar', NULL, 100, 5, 3, 1),
+('there and back again', 'thx a lot, deliver this ancinet sword back', NULL, 100, 5, 3, 2)
+go
+
+insert into locations(name, x_coord, y_coord, fraction) values
+('Hordes territory', 250, 300, 2),
+('Crater Ungoro', 100, 150, 2),
+('Fields of lightness', 1000, 1500, 1),
+('Fire mountains', 600, 750, 3),
+('Iced lakes', 600, 750, 3)
+go
+
+insert into cities(name, location, fraction) values
+('Ogrimar', 2, 2),
+('Trolden', 2, 2),
+('Stormgrad', 3, 1),
+('Stalgorn', 3, 1),
+('Dalaran', 4, 3)
+go
+
+
+
+insert into npc(name, lvl, location) values
+('Mayor of Ogrimar', 150, 1),
+('Mayor of Trolden', 150, 2),
+('Blacksmith of Stormgrad', 150, 3),
+('Cunning guardian', 50, 5),
+('Shy thief', 35, 4)
+go
+
+insert into auctions(name, city) values
+('OggAuc', 1),
+('TrolAuc', 2),
+('StromAcu', 3),
+('StalAcu', 4),
+('The great auction of Dalaran', 5)
+go
+
+insert into shops(name, city) values
+('Staff and food', 1),
+('Murrrggglll', 2),
+('Lightning', 1),
+('Steel and Honor', 1),
+('Market', 1)
+go
+
+insert into banks(name, net_name, multifraction, city) values
+('OggBank', 'hordes banks', 0, 1),
+('TroBank', 'hordes banks', 0, 1),
+('Sberbank', 'just banks', 1, 5),
+('Alfabank', 'just banks', 1, 4),
+('LightOfWisdom', 'aliance banks', 0, 3)
+go
+
+insert into dungeons(name, min_lvl, location, achievement) values
+('strange cave', 15, 2, 2),
+('very strange cave', 15, 2, null),
+('old castle', 25, 3, null),
+('old cemetery', 25, 1, null),
+('dragons mountain', 50, 4, 5)
+go
+
+insert into teachers(name, lvl, city) values
+('master shi-fu', 50, 4),
+('witch lady', 50, 1),
+('Gendalf', 500, 5),
+('Ip Man', 50, 5),
+('Tatyana Ivanovna', 500, 1),
+('midget dwarf', 15, 1),
+('old jew', 15, 1),
+('Legolas', 50, 3)
+go
+
+insert into professions(name, teacher) values
+('blacksmithing', 6),
+('alchemy', 2),
+('jewelry', 7),
+('mining', 6),
+('maths', 5)
+go
+
+insert into classes(name, teacher) values
+('mage', 3),
+('warrior', 4),
+('rogue', 1),
+('archer', 8),
+('necromancy', 2)
+go
+
+insert into races_classes_compatibility(race, class) values
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(2, 1),
+(2, 5),
+(3, 2),
+(4, 5),
+(5, 2),
+(6, 3),
+(6, 4)
+go
+
+insert into guilds(name, fraction) values
+('Newbies', 3),
+('For Horde', 1),
+('Holy Light', 2),
+('PVP only', 3),
+('test', 4)
+go
+
+insert into characters(name, race, class, health, attack, defense, experience, level_, inventory, guild, fraction) values
+('TopOdinPaladin', 1, 2, 10000, 5000, 5000, 2694536178, 50, 1, 3, 1),
+('Nagibator', 1, 2, 150, 11, 24, 74, 7, 2, null, 2),
+('Maksim', 6, 3, 150, 11, 24, 74, 7, 2, null, 2),
+('AnotherMaksim', 6, 3, 150, 11, 24, 74, 7, 2, 2, 2),
+('WhoIsMaksim', 6, 3, 150, 11, 24, 74, 7, 2, 2, 2)
+go
+
+insert into exp_lvl_relation(exp_, lvl) values
+(0, 1),
+(10, 2),
+(15, 3),
+(22, 4),
+(33, 5),
+(49, 6),
+(73, 7),
+(109, 8),
+(163, 9),
+(244, 10),
+(366, 11),
+(549, 12),
+(823, 13),
+(1234, 14),
+(1851, 15),
+(2776, 16),
+(4164, 17),
+(6246, 18),
+(9369, 19),
+(14053, 20),
+(21079, 21),
+(31618, 22),
+(47427, 23),
+(71140, 24),
+(106710, 25),
+(160065, 26),
+(240097, 27),
+(360145, 28),
+(540217, 29),
+(810325, 30),
+(1215487, 31),
+(1823230, 32),
+(2734845, 33),
+(4102267, 34),
+(6153400, 35),
+(9230100, 36),
+(13845150, 37),
+(20767725, 38),
+(31151587, 39),
+(46727380, 40),
+(70091070, 41),
+(105136605, 42),
+(157704907, 43),
+(236557360, 44),
+(354836040, 45),
+(532254060, 46),
+(798381090, 47),
+(1197571635, 48),
+(1796357452, 49),
+(2694536178, 50)
+go
+
+insert into raid_groups(name, members_count, dungeon, leader) values
+('need dd', 1, 1, 1),
+('heal?', 1, 1, 2),
+('maksims', 3, 3, 3)
+go
+
+insert into talants_on_characters(character_, talant) values
+(1, 1),
+(1, 3),
+(2, 2),
+(3, 4),
+(3, 5),
+(4, 4),
+(4, 5),
+(5, 4),
+(5, 5)
+go
+
+insert into quest_line_on_npc(quest_line, npc) values
+(1, 1),
+(2, 1),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into achievs_on_account(account, achievement) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into items_on_monster(monster, item) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into cell_in_bank(character_, bank) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into items_in_cell(cell, item) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into items_in_inventory(inv, item, durability_, on_char) values
+(1, 1, 100, 1),
+(2, 1, 100, 1),
+(3, 1, 100, 1),
+(4, 1, 100, 1),
+(5, 1, 100, 1)
+go
+
+insert into chars_on_account(account, character_) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into skills_on_char(char_, skill) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into skills_on_class(class, skill) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into profs_on_char(char_, profession) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into skills_on_teacher(teacher, skill) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into skills_on_profession(profession, skill) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into items_in_shop(shop, item, price) values
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 5)
+go
+
+insert into chars_in_raid(raid, char_) values
+(1, 1),
+(2, 2),
+(3, 3),
+(3, 4),
+(3, 5)
+go
+
+insert into monsters_in_dungeon(dungeon, monster) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5)
+go
+
+insert into items_on_auction(auction, item, start_price, current_price, ransom_price, last_better, owner_) values
+(1,1,1,1,2,2,1),
+(1,2,2,2,3,2,1),
+(1,3,3,3,4,2,1),
+(1,4,4,4,5,2,1),
+(1,5,5,5,6,2,1)
+go
---
+++ tables_create.sql
@@ -0,0 +1,440 @@
+use mmorpg;
+
+exec drop_all_tables
+
+create table races(
+id int primary key identity(1,1),
+name varchar(max) not null
+);
+go
+
+create table fractions(
+id int primary key identity(1,1),
+name varchar(max) not null
+);
+go
+
+create table talants(
+id int primary key identity(1,1),
+name varchar(max) not null,
+min_lvl int
+);
+go
+
+create table skills(
+id int primary key identity(1,1),
+name varchar(max) not null,
+min_lvl int
+);
+go
+
+create table inventories(
+id int primary key identity(1,1),
+size int not null,
+fullness int
+);
+go
+
+create table item_types(
+id int primary key identity(1,1),
+type_ varchar(max),
+feauture_name varchar(max)
+);
+go
+
+create table achievements (
+id int primary key identity(1,1),
+name varchar(max) not null,
+--percentage_of_completion int
+);
+go
+
+create table quest_lines(
+id int primary key identity(1,1),
+name varchar(max) not null,
+description_ varchar(max) not null
+);
+go
+
+create table accounts (
+id int primary key identity(1,1),
+login_ varchar(max) not null,
+password_ varchar(max) not null,
+characters_count int,
+max_characters int not null,
+percentage_of_achievements float
+);
+go
+
+create table monsters (
+id int primary key identity(1,1),
+name varchar(max) not null,
+lvl int not null,
+health int not null,
+attack int,
+defense int,
+--location int not null,
+
+--constraint fk_monsters_location foreign key(location) references locations(id)
+);
+go
+
+-- —¬≈–’” ¬—® ¡≈« ——€ÀŒ 
+
+
+create table items(
+id int primary key identity(1,1),
+name varchar(max) not null,
+type_ int not null,
+feature_value int,
+min_lvl int,
+
+constraint fk_items_type foreign key(type_) references item_types(id)
+--constraint fk_items_type foreign key(type_) references item_types(type_)
+);
+go
+
+create table quests (
+id int primary key identity(1,1),
+name varchar(max) not null,
+description_ varchar(max) not null,
+reward_item int,
+reward_money int,
+lvl int,
+quest_line int,
+number_in_quest_line int,
+
+constraint fk_quests_reward_item foreign key(reward_item) references items(id),
+constraint fk_quests_quest_line foreign key(quest_line) references quest_lines(id)
+);
+go
+
+create table locations(
+id int primary key identity(1,1),
+name varchar(max) not null,
+x_coord int not null,
+y_coord int not null,
+fraction int not null,
+
+constraint fk_locations_fraction foreign key(fraction) references fractions(id)
+);
+go
+
+create table cities (
+id int primary key identity(1,1),
+name varchar(max) not null,
+location int not null,
+fraction int not null,
+
+constraint fk_cities_location foreign key(location) references locations(id),
+constraint fk_cities_fraction foreign key(fraction) references fractions(id)
+);
+go
+
+-- Œ“—ﬁƒ¿ ÃŒ∆ÕŒ ¬—® — √Œ–Œƒ¿Ã» » ÀŒ ¿÷»ﬂÃ»
+
+
+create table npc (
+id int primary key identity(1,1),
+name varchar(max) not null,
+lvl int not null,
+location int,
+
+constraint fk_npc_location foreign key(location) references locations(id)
+);
+go
+
+create table auctions(
+id int primary key identity(1,1),
+name varchar(max) not null,
+city int
+
+constraint fk_auctions_city foreign key(city) references cities(id)
+);
+go
+
+create table shops(
+id int primary key identity(1,1),
+name varchar(max) not null,
+city int
+
+constraint fk_shop_city foreign key(city) references cities(id)
+);
+go
+
+create table banks (
+id int primary key identity(1,1),
+name varchar(max) not null,
+net_name varchar(max) not null,
+multifraction bit,
+city int not null,
+
+constraint fk_banks_city foreign key(city) references cities(id)
+);
+go
+
+create table dungeons (
+id int primary key identity(1,1),
+name varchar(max) not null,
+min_lvl int,
+location int,
+achievement int,
+
+constraint fk_dungeons_location foreign key(location) references locations(id),
+constraint fk_dungeons_achievement foreign key(achievement) references achievements(id)
+);
+go
+
+create table teachers(
+id int primary key identity(1,1),
+name varchar(max) not null,
+lvl int not null,
+city int not null,
+
+constraint fk_teachers_city foreign key(city) references cities(id)
+);
+go
+
+create table professions (
+id int primary key identity(1,1),
+name varchar(max) not null,
+teacher int not null,
+
+constraint fk_professions_teacher foreign key(teacher) references teachers(id)
+);
+go
+
+create table classes(
+id int primary key identity(1,1),
+name varchar(max),
+teacher int not null
+
+constraint fk_classes_teacher foreign key(teacher) references teachers(id)
+);
+go
+
+create table races_classes_compatibility (
+race int not null,
+class int not null,
+
+constraint fk_r_c_comp_race foreign key(race) references races(id),
+constraint fk_r_c_comp_class foreign key(class) references classes(id),
+);
+go
+
+create table guilds(
+id int primary key identity(1,1),
+name varchar(max) not null,
+fraction int not null,
+
+constraint fk_guild_fraction foreign key(fraction) references fractions(id)
+);
+go
+
+create table characters (
+id int primary key identity(1,1),
+name varchar(max) not null,
+race int not null,
+class int not null,
+
+health int not null,
+attack int,
+defense int,
+
+experience bigint,
+level_ int not null,
+inventory int not null,
+guild int,
+fraction int,
+
+constraint fk_characters_race foreign key(race) references races(id),
+constraint fk_characters_class foreign key(class) references classes(id),
+constraint fk_characters_inventory foreign key(inventory) references inventories(id),
+constraint fk_characters_guild foreign key(guild) references guilds(id),
+constraint fk_characters_fraction foreign key(fraction) references fractions(id),
+);
+go
+
+create table exp_lvl_relation(
+exp_ bigint,
+lvl int not null
+);
+go
+
+create table raid_groups (
+id int primary key identity(1,1),
+name varchar(max) not null,
+members_count int,
+dungeon int not null,
+leader int not null,
+
+constraint fk_raid_dungeon foreign key(dungeon) references dungeons(id),
+constraint fk_raid_leader foreign key(leader) references characters(id),
+);
+go
+
+-- —¬ﬂ«”ﬁŸ»≈ «ƒ≈—‹
+create table talants_on_characters(
+character_ int not null,
+talant int not null,
+
+constraint fk_tal_on_char_char foreign key(character_) references characters(id),
+constraint fk_tal_on_char_tal foreign key(talant) references talants(id)
+);
+go
+
+create table quest_line_on_npc(
+quest_line int not null,
+npc int not null,
+
+constraint fk_qu_l_on_npc_ql foreign key(quest_line) references quest_lines(id),
+constraint fk_qu_l_on_npc_npc foreign key(npc) references npc(id)
+);
+go
+
+create table achievs_on_account(
+account int not null,
+achievement int not null,
+
+constraint fk_ach_on_acc_acc foreign key(account) references accounts(id),
+constraint fk_ach_on_acc_ach foreign key(achievement) references achievements(id)
+);
+go
+
+create table items_on_monster(
+monster int not null,
+item int not null,
+
+constraint fk_it_on_mon_monster foreign key(monster) references monsters(id),
+constraint fk_it_on_mon_item foreign key(item) references items(id)
+);
+go
+
+create table cell_in_bank(
+id int primary key identity(1,1),
+character_ int not null,
+bank int not null,
+
+constraint fk_cell_in_bank_char foreign key(character_) references characters(id),
+constraint fk_cell_in_bank_bank foreign key(bank) references banks(id)
+);
+go
+
+create table items_in_cell(
+cell int not null,
+item int not null,
+
+constraint fk_items_in_cell_cell foreign key(cell) references cell_in_bank(id),
+constraint fk_items_in_cell_item foreign key(item) references items(id)
+);
+go
+
+create table items_in_inventory(
+inv int not null,
+item int not null,
+
+durability_ int,
+on_char bit,
+
+constraint fk_items_in_inv_item foreign key(item) references items(id),
+constraint fk_items_in_inv_inv foreign key(inv) references inventories(id)
+);
+go
+
+create table chars_on_account(
+character_ int not null,
+account int not null,
+
+constraint fk_chars_on_account_char foreign key(character_) references characters(id),
+constraint fk_chars_on_account_acc foreign key(account) references accounts(id)
+);
+go
+
+create table skills_on_char (
+char_ int not null,
+skill int not null,
+
+constraint fk_skills_on_char_char foreign key(char_) references characters(id),
+constraint fk_skills_on_char_skill foreign key(skill) references skills(id),
+);
+go
+
+create table skills_on_class (
+class int not null,
+skill int not null,
+
+constraint fk_skills_on_class_char foreign key(class) references classes(id),
+constraint fk_skills_on_class_skill foreign key(skill) references skills(id),
+);
+go
+
+create table profs_on_char (
+char_ int not null,
+profession int not null,
+
+constraint fk_profs_on_char_char foreign key(char_) references characters(id),
+constraint fk_profs_on_char_prof foreign key(profession) references professions(id),
+);
+go
+
+create table skills_on_teacher(
+teacher int not null,
+skill int not null,
+
+constraint fk_skills_on_teacher_teacher foreign key(teacher) references teachers(id),
+constraint fk_skills_on_teacher_skill foreign key(skill) references skills(id)
+);
+go
+
+create table skills_on_profession(
+profession int not null,
+skill int not null,
+
+constraint fk_skills_on_profession_profession foreign key(profession) references professions(id),
+constraint fk_skills_on_profession_skill foreign key(skill) references skills(id)
+);
+go
+
+create table items_in_shop(
+item int not null,
+shop int not null,
+price int not null,
+
+constraint fk_items_in_shop_item foreign key(item) references items(id),
+constraint fk_items_in_shop_shop foreign key(shop) references shops(id)
+);
+go
+
+create table chars_in_raid(
+char_ int not null,
+raid int not null,
+
+constraint fk_char_on_raid_char foreign key(char_) references characters(id),
+constraint fk_char_on_raid_raid foreign key(raid) references raid_groups(id)
+);
+go
+
+create table monsters_in_dungeon(
+monster int not null,
+dungeon int not null,
+
+constraint fk_monsters_in_dungeon_monster foreign key(monster) references monsters(id),
+constraint fk_monsters_in_dungeon_dungeon foreign key(dungeon) references dungeons(id)
+);
+go
+
+create table items_on_auction(
+item int not null,
+auction int not null,
+start_price int not null,
+current_price int not null,
+ransom_price int not null,
+last_better int,
+owner_ int not null,
+
+constraint fk_items_on_auction_item foreign key(item) references items(id),
+constraint fk_items_on_auction_autcion foreign key(auction) references auctions(id),
+constraint fk_items_on_auction_last_better foreign key(last_better) references characters(id),
+constraint fk_items_on_auction_owner foreign key(owner_) references characters(id),
+);
+go
